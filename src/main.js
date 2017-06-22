7ce8fc7e0af11b1516bb7946b79b0ddc1ee56718
Elias Melgaço <eliasmelgaco@gmail.com>
2017-06-22 17:00:56 -0300

    reademe/styleguides/eslint/npm/hooks

---
+++ src/main.js
@@ -0,0 +1,8 @@
+const a = 3;
+const b = 2;
+const c = 'askgjdalksjdglkajsdklgjalksdjgalksjdglkajsdglkajsdglkajsdlkgjals;dgjal;skdjgal;ksdjgla;ksdjglak;sjgdkl;asjdgl;kajsdglkajsdlgkjasgd';
+agsdasgs
+function sum() {
+  return a + b + c;
+}
+sum();
