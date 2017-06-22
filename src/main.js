e0f15ccbb79b54287931c925400f3de73dfc4923
Elias Melgaço <eliasmelgaco@gmail.com>
2017-06-22 17:03:19 -0300

    Fix main

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 const a = 3;
 const b = 2;
 const c = 'askgjdalksjdglkajsdklgjalksdjgalksjdglkajsdglkajsdglkajsdlkgjals;dgjal;skdjgal;ksdjgla;ksdjglak;sjgdkl;asjdgl;kajsdglkajsdlgkjasgd';
-agsdasgs
+
 function sum() {
   return a + b + c;
 }
